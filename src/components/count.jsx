12657184d51a0b06f30255a56b1ab5ec1a4df328
mohamadmahdi <135762439+mohamadhosseinpour16@users.noreply.github.com>
2024-08-21 15:48:07 +0330

    write app using react hook useState

--- src/components/count.jsx
+++ src/components/count.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import classes from "./count.module.css";
 const Counter = () => {
+  const [state, setState] = useState(0);
+  // functions
+  const addCount = () => {
+    setState((prevState) => {
+      return prevState + 1;
+    });
+  };
+  const minusCount = () => {
+    setState((prevState) => {
+      return prevState - 1;
+    });
+  };
+  const oddCount = () => {
+    if (state % 2 === 1) {
+      setState((prevState) => {
+        return prevState + 1;
+      });
+    }
+  };
   return (
     <main className={classes.primary}>
       <aside>
         <h1>counter app</h1>
         <h2>
-          count :<span>0</span>
+          count : <span>{state}</span>
         </h2>
       </aside>
       <nav>
-        <button>add</button>
-        <button>minus</button>
-        <button>add when odd</button>
+        <button onClick={addCount}>add</button>
+        <button onClick={minusCount}>minus</button>
+        <button onClick={oddCount}>add when odd</button>
       </nav>
     </main>
   );
